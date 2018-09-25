743f1c88d8b62533a0293d5404539da83cbd815f
ahmedharazi <ahmedharazi@github.com>
2018-09-25 16:31:08 -0400

    fixthefrontend

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,100 +1,103 @@
-var cars = ['BMW', 'MAZADA', 'HONDA'
-];
+var cars = ['BMW', 'MAZDA', 'Dodge'];
 
-function displaycarInfo(){
+function displaycarInfo() {
 
-  $('#carsView').empty();     
+    $('#carsView').empty();
 
-  var car = $(this).attr('data-name');
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var car = $(this).attr('data-name');
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-  $.ajax({url: queryURL, method: 'GET'})
-     .done(function(response) {
-         var results = response.data;
+    $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function (response) {
+            console.log(response)
+            var results = response.data;
 
-         for(var i=0; i < results.length; i++){
+            for (var i = 0; i < results.length; i++) {
 
 
-            if (results[i].rating == "r" || results[i].rating == "r")
-            {
+                if (results[i].rating == "r" || results[i].rating == "r") {
+                    console.log("r", results)
+
+                } else {
+
+
+                    var rating = results[i].rating;
+
+                    var p = $('<p>').text("Rating: " + rating);
+                   
+
+                    var carImage = $('<img>');
+                    // carImage.addClass('card-img-top');
+                    carImage.attr('src', results[i].images.fixed_height_still.url);
+                    carImage.attr('data-still', results[i].images.fixed_height_still.url);
+                    carImage.attr('data-animate', results[i].images.fixed_height.url);
+                    carImage.attr('data-state', 'still');
+                    carImage.addClass('carImage');
+
+                    $('#carsView').append(p);
+                    $('#carsView').append(carImage);
+
+
+                }
 
             }
-            else {
 
-             console.log(response)
+            $('.carImage').on('click', function () {
+                var state = $(this).attr('data-state');
+                console.log(state);
 
-             var rating = results[i].rating;
+                if (state == 'still') {
+                    $(this).attr('src', $(this).data('animate'));
+                    $(this).attr('data-state', 'animate');
+                } else {
+                    $(this).attr('src', $(this).data('still'));
+                    $(this).attr('data-state', 'still');
+                }
 
-             var p = $('<p>').text( "Rating: " + rating);
-
-             var carImage = $('<img>');
-             carImage.attr('src', results[i].images.fixed_height_still.url);
-             carImage.attr('data-still', results[i].images.fixed_height_still.url);
-             carImage.attr('data-animate', results[i].images.fixed_height.url);
-             carImage.attr('data-state', 'still');
-             carImage.addClass('carImage');
-             
-             $('#carsView').append(p);
-             $('#carsView').append(carImage);
+            });
 
 
-            }
-
-         }
-
-    $('.carImage').on('click', function(){
-        var state = $(this).attr('data-state'); 
-          console.log(state);
-       
-          if ( state == 'still'){
-              $(this).attr('src', $(this).data('animate'));
-              $(this).attr('data-state', 'animate');
-          }else{
-              $(this).attr('src', $(this).data('still'));
-              $(this).attr('data-state', 'still');
-          }
-        
-    });
-
-      
-    });   
+        });
 
 }
 
 
-function renderButtons(){ 
+function renderButtons() {
 
 
-  $('#buttonsView').empty();
+    $('#buttonsView').empty();
 
 
-  for (var i = 0; i < cars.length; i++){
+    for (var i = 0; i < cars.length; i++) {
 
-   
-      var a = $('<button>') 
-      a.addClass('car'); 
-      a.addClass("btn btn-success"); 
-      a.addClass("btn btn-primary btn-lg");
-      a.attr('data-name', cars[i]); 
-      a.text(cars[i]); 
-      $('#buttonsView').append(a); 
-  }
+
+        var a = $('<button>')
+        a.addClass('car');
+        a.addClass("btn btn-success");
+        a.addClass("btn btn-primary btn-lg");
+        a.attr('data-name', cars[i]);
+        a.text(cars[i]);
+        $('#buttonsView').append(a);
+    }
 }
 
 
-$('#addcar').on('click', function(){
-
-  
-  var car = $('#car-input').val().trim();
+$('#addcar').on('click', function () {
 
 
-  cars.push(car);
-  
-
-  renderButtons();
+    var car = $('#car-input').val().trim();
 
 
-  return false;
+    cars.push(car);
+
+
+    renderButtons();
+
+
+    return false;
 })
 
 
